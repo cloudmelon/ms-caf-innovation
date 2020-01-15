cab0fb53344fa112b6dc70f68b800ea92dd83c8b
cloudmelon <urinaway@gmail.com>
2020-01-15 23:59:23 +0000

    add az policy governance

---
+++ scripts/aks-policy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# get your AKS onboard the Azure Policy
+az aks enable-addons --addons azure-policy --name MyAKSCluster --resource-group MyResourceGroup
+
+# build-in policy https://docs.microsoft.com/en-us/azure/governance/policy/concepts/rego-for-aks#built-in-policies
+#az aks disable-addons --addons azure-policy --name MyAKSCluster --resource-group MyResourceGroup
